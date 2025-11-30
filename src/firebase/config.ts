0fef17c292a3c8c4893a927ca42f465919632182
Torneo Mojarrero <torneomojarrero@gmail.com>
2025-11-30 22:23:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8742736054-c982d",
+  "appId": "1:985691537420:web:ca11dd045f74b0aeaaee29",
+  "apiKey": "AIzaSyDcKZQw0wEK-QkuspzCto2RItYN7roLtpk",
+  "authDomain": "studio-8742736054-c982d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "985691537420"
+};
