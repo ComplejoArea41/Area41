c28406d85327f83f4d44c9f36252d871db2e4c0b
Torneo Mojarrero <torneomojarrero@gmail.com>
2025-12-01 02:00:50 +0000

    q los cuadrados sean mas chicos

--- src/app/buffet/page.tsx
+++ src/app/buffet/page.tsx
@@ -28,7 +28,7 @@ export default function BuffetPage() {
               </CardDescription>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
             {menuItems.map((item) => {
                 const image = placeholderImages.find(p => p.id === item.imageId);
                 return (
