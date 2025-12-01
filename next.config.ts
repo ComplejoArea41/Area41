57c87be30e895c1a3e11cb4e918232d6845d8841
Torneo Mojarrero <torneomojarrero@gmail.com>
2025-12-01 02:15:19 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.lanacion.com.ar',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
