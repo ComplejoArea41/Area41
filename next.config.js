469f8afa6cfcb19fbc0e2bcc2d144a2307481a80
Torneo Mojarrero <torneomojarrero@gmail.com>
2025-12-10 03:10:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,24 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  serverActions: {
+    bodySizeLimit: '2mb', // Default is 1mb, increased for safety
+  },
+};
+
+module.exports = nextConfig;
