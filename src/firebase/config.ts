e18ab0d4cc999d15d7a917b501e7245777bca905
Torneo Mojarrero <torneomojarrero@gmail.com>
2025-12-10 02:52:38 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8742736054-c982d",
   "appId": "1:985691537420:web:ca11dd045f74b0aeaaee29",
+  "storageBucket": "studio-8742736054-c982d.appspot.com",
   "apiKey": "AIzaSyDcKZQw0wEK-QkuspzCto2RItYN7roLtpk",
   "authDomain": "studio-8742736054-c982d.firebaseapp.com",
   "measurementId": "",
