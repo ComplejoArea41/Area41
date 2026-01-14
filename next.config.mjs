d228ea6f3fbf7a4f45447cbcaeb6448a7d332603
Torneo Mojarrero <torneomojarrero@gmail.com>
2026-01-14 16:40:05 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -5,35 +5,57 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'storage.googleapis.com',
+                port: '',
+                pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'photos.fife.usercontent.google.com',
+                port: '',
+                pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'i.ibb.co',
+                port: '',
+                pathname: '/**',
             },
-             {
+            {
                 protocol: 'https',
                 hostname: 'ibb.co',
+                port: '',
+                pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'www.imghippo.com',
+                port: '',
+                pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'www.instagram.com',
+                port: '',
+                pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'drive.google.com',
-            }
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
 };
