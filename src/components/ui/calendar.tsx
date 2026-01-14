21bdef8e5e4248e6e2ca7a0bf312f35a1b6f713c
Torneo Mojarrero <torneomojarrero@gmail.com>
2026-01-14 04:20:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,8 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: (props) => <ChevronLeft className="h-4 w-4" {...props} />,
-        IconRight: (props) => <ChevronRight className="h-4 w-4" {...props} />,
+        Chevron: ({ orientation, ...props }) => {
+          const Icon = orientation === "left" ? ChevronLeft : ChevronRight;
+          return <Icon className={cn("h-4 w-4", props.className)} {...props} />;
+        },
       }}
       {...props}
     />
