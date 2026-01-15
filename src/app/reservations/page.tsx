0d75e7da3ae6ad9a6c1cd935e883a7025f4e6e66
Torneo Mojarrero <torneomojarrero@gmail.com>
2026-01-15 00:40:30 +0000

    sigue sin aparecer

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -410,6 +410,7 @@ export default function ReservationPage() {
                   )}
                 />
                 
+                {/* 
                 <div className="space-y-4">
                     <FormLabel className="text-base font-semibold">2. Elige la fecha</FormLabel>
                     <FormField
@@ -440,6 +441,7 @@ export default function ReservationPage() {
                         )}
                     />
                 </div>
+                 */}
                 
                 <div className="space-y-4">
                     <FormLabel className="text-base font-semibold">3. Elige el horario</FormLabel>
