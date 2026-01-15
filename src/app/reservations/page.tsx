d5e2f990ca943ca1e5c552dbff6a862f0d8e8050
Torneo Mojarrero <torneomojarrero@gmail.com>
2026-01-15 00:42:13 +0000

    sigue sin cargar la web

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -11,7 +11,7 @@ import { collection, query, where, Timestamp, doc, addDoc }from 'firebase/firest
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+// import { Calendar } from "@/components/ui/calendar";
 import {
   Card,
   CardContent,
