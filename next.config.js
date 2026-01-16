b6bb950d12937b3117e537d5a1a6719ade5ec57c
Torneo Mojarrero <torneomojarrero@gmail.com>
2026-01-16 01:31:23 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
