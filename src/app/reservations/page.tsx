07e527dbd8bbdf533816e81cf5dc0e57c9e15099
Torneo Mojarrero <torneomojarrero@gmail.com>
2026-01-19 15:56:34 +0000

    buenas tardes la app funciona perfecto .. por ahora el unico error q ten

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useMemo, useCallback, useEffect } from 'react';
 import { addDays, format, startOfDay, isBefore, set, isSameDay } from 'date-fns';
-import { es } from 'date-fns/locale/es';
+import { es } from 'date-fns/locale';
 import { collection, query, where, Timestamp, doc, addDoc } from 'firebase/firestore';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
@@ -235,7 +235,7 @@ export default function ReservationPage() {
     );
   
     const whatsappUrl = `https://wa.me/2324610433?text=${message}`;
-    window.open(whatsappUrl, '_blank');
+    window.location.assign(whatsappUrl);
   
     setIsDialogOpen(false);
   }
